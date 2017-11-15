21638ab601a0fce123430d824b9b665b22c63942
Eric McKinney <mckinney99@gmail.com>
2017-11-15 00:02:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
       #course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
-    
+
     end
 
     def get_courses
@@ -40,7 +40,7 @@ class Scraper
     course.description = post.css("p").text
   end
 end
-    
+
 end
 
 Scraper.new.get_page
